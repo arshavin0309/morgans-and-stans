711b16496d8cec9cb6b643e72e99f55bbbaadb8b
Владислав <arshavin0309@mail.ru>
2024-06-19 16:52:56 +0500

    add

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // появление хедера при скролле
     let body = document.querySelector("body");
     let header = document.querySelector(".header");
 
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // свайпер
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         loop: true,
@@ -19,6 +21,11 @@ document.addEventListener("DOMContentLoaded", () => {
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
+        },
+
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
         },
 
         // Navigation arrows
@@ -49,4 +56,107 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
+
+    // закрытие всех details при открытии нового details
+    // Fetch all the details element.
+    const details = document.querySelectorAll("details");
+
+    // Add the onclick listeners.
+    details.forEach((detail) => {
+        detail.addEventListener("toggle", () => {
+            if (detail.open) setTargetDetail(detail);
+        });
+    });
+
+    // Close all the details that are not targetDetail.
+    function setTargetDetail(targetDetail) {
+        details.forEach((detail) => {
+            if (detail !== targetDetail) {
+                detail.open = false;
+            }
+        });
+    }
+
+    // табы
+
+    let btn = document.querySelector('.assets__btn')
+
+    document.querySelector('#tab-1').addEventListener('click', () => {
+        let tables = document.querySelectorAll('.assets__box .assets__table')
+
+        for (let i = 0; i < tables.length; i++) {
+            tables[i].style = 'display: none'
+            tables[i].classList.remove('table-active')
+        }
+
+        document.querySelector('.tab-1').style = 'display: table'
+        document.querySelector('.tab-1').classList.add('table-active')
+        btn.textContent = 'Показать еще'
+    })
+
+    document.querySelector('#tab-2').addEventListener('click', () => {
+        let tables = document.querySelectorAll('.assets__box .assets__table')
+
+        for (let i = 0; i < tables.length; i++) {
+            tables[i].style = 'display: none'
+            tables[i].classList.remove('table-active')
+        }
+
+        document.querySelector('.tab-2').style = 'display: table'
+        document.querySelector('.tab-2').classList.add('table-active')
+        btn.textContent = 'Показать еще'
+    })
+
+    document.querySelector('#tab-3').addEventListener('click', () => {
+        let tables = document.querySelectorAll('.assets__box .assets__table')
+
+        for (let i = 0; i < tables.length; i++) {
+            tables[i].style = 'display: none'
+            tables[i].classList.remove('table-active')
+            btn.textContent = 'Показать еще'
+        }
+
+        document.querySelector('.tab-3').style = 'display: table'
+        document.querySelector('.tab-3').classList.add('table-active')
+    })
+
+    document.querySelector('#tab-4').addEventListener('click', () => {
+        let tables = document.querySelectorAll('.assets__box .assets__table')
+
+        for (let i = 0; i < tables.length; i++) {
+            tables[i].style = 'display: none'
+            tables[i].classList.remove('table-active')
+            btn.textContent = 'Показать еще'
+        }
+
+        document.querySelector('.tab-4').style = 'display: table'
+        document.querySelector('.tab-4').classList.add('table-active')
+    })
+
+    document.querySelector('#tab-5').addEventListener('click', () => {
+        let tables = document.querySelectorAll('.assets__box .assets__table')
+
+        for (let i = 0; i < tables.length; i++) {
+            tables[i].style = 'display: none'
+            tables[i].classList.remove('table-active')
+            btn.textContent = 'Показать еще'
+        }
+
+        document.querySelector('.tab-5').style = 'display: table'
+        document.querySelector('.tab-5').classList.add('table-active')
+    })
+
+    btn.addEventListener('click', () => {
+        let table = document.querySelector('.table-active')
+
+        if (btn.textContent === 'Показать еще') {
+            table.classList.add('show')
+            btn.classList.add('show')
+            btn.textContent = 'Скрыть'
+        } else {
+            table.classList.remove('show')
+            btn.classList.remove('show')
+            btn.textContent ='Показать еще'
+        }
+    })
 })
