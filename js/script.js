e8f4891eb3f361d76aee64d3e6338cef30610891
Владислав <arshavin0309@mail.ru>
2024-06-27 14:12:56 +0500

    add

--- js/script.js
+++ js/script.js
@@ -226,7 +226,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (let i = 0; i < headerItem.length; i++) {
         headerItem[i].addEventListener('click', () => {
-            
+
             for (let i = 0; i < headerItem.length; i++) {
                 headerItem[i].classList.remove('show')
             }
@@ -234,4 +234,49 @@ document.addEventListener("DOMContentLoaded", () => {
             headerItem[i].classList.toggle('show')
         })
     }
+
+    function getCookie(name) {
+        let matches = document.cookie.match(
+            new RegExp(
+                '(?:^|; )' +
+                name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') +
+                '=([^;]*)'
+            )
+        )
+        return matches ? decodeURIComponent(matches[1]) : undefined
+    }
+    
+    function setCookie(name, value, options = {}) {
+        options = {
+            path: '/',
+            ...options,
+        }
+    
+        if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString()
+        }
+    
+        let updatedCookie =
+            encodeURIComponent(name) + '=' + encodeURIComponent(value)
+    
+        for (let optionKey in options) {
+            updatedCookie += '; ' + optionKey
+            let optionValue = options[optionKey]
+            if (optionValue !== true) {
+                updatedCookie += '=' + optionValue
+            }
+        }
+    
+        document.cookie = updatedCookie
+    }
+    
+    
+    if (!getCookie('cookies')) {
+        document.querySelector('.cookies').style = 'display: flex'
+    }
+
+    document.querySelector('.cookies .btn').addEventListener('click', () => {
+        document.querySelector('.cookies').style = 'display: none'
+        setCookie('cookies', 'true', { 'max-age': 3600 * 24 * 365 })
+    })
 })
